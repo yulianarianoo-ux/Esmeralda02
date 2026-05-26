c9e180ff875b881106b900389d22586fb02c2a90
Esmeralda Obregon <yuliana.rianoo@colpre.edu.co>
2026-05-25 22:03:53 -0500

    gasdseS

--- Logica/SantiL.cs
+++ Logica/SantiL.cs
@@ -7,5 +7,60 @@ namespace Esmeralda02.Logica
 {
     public class SantiL
     {
+        public List<Mascota> MtBuscarMascotas(string texto)
+        {
+            List<Mascota> oBusquedadMascotas = new List<Mascota>();
+            using (SqlConnection cn = ConexionDB.MtAbrirConexion())
+            {
+                cn.Open();
+                string query = @"
+                    SELECT 
+                        m.Id,
+                        m.NombreMascota,
+                        m.Especie,
+                        m.Raza,
+                        m.Sexo,
+                        m.Edad,
+                        p.Nombre + ' ' + p.Apellido AS Propietario
+                    FROM Mascota m
+                    INNER JOIN Persona p ON m.IdPersona = p.Id
+                    WHERE m.NombreMascota LIKE @buscar OR p.Nombre LIKE @buscar";
+                using (SqlCommand cmd = new SqlCommand(query, cn))
+                {
+                    cmd.Parameters.AddWithValue("@buscar", "%" + texto + "%");
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+
+                            Mascota oMascota = new Mascota()
+                            {
+
+                                Id = Convert.ToInt32(dr["Id"]),
+                                NombreMascota = dr["NombreMascota"].ToString(),
+                                Especie = dr["Especie"].ToString(),
+                                Raza = dr["Raza"].ToString(),
+                                Sexo = dr["Sexo"].ToString(),
+                                Edad = Convert.ToInt32(dr["Edad"]),
+
+                                Persona = new Persona
+                                {
+                                    Nombre = dr["Nombre"].ToString(),
+                                    Apellido = dr["Apellido"].ToString()
+                                }
+
+                            };
+                            oBusquedadMascotas.Add(oMascota);
+                        }
+                        return oBusquedadMascotas;
+                    }
+                }
+            }
+        }
     }
 }
+
+
+
+
+  
